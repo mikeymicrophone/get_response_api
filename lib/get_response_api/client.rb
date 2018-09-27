390d6ed24b526d96bcc534b60acae6fad34359af
Mike Schwab <mike.schwab@gmail.com>
2018-09-26 21:26:55 -0400

    contacts can be retrieved, filtered by campaign or email

--- lib/get_response_api/client.rb
+++ lib/get_response_api/client.rb
@@ -11,6 +11,16 @@ module GetResponseApi
     def campaigns(page: 1, per_page: 250)
       @connection.request(:get, "/campaigns?page=#{page}&perPage=#{per_page}")
     end
+    
+    def contacts options = {}
+      if options[:campaign_id]
+        @connection.request(:get, "/contacts?query[campaignId]=#{options[:campaign_id]}")
+      elsif options[:email_address]
+        @connection.request(:get, "/contacts?query[email]=#{options[:email_address]}")
+      else
+        @connection.request(:get, '/contacts')
+      end
+    end
 
     def custom_fields
       @connection.request(:get, '/custom-fields')
